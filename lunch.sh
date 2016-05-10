ab4df9ad13257afef67c7618301519031338d373
hantek <lin.zhouhan@gmail.com>
2016-05-09 22:22:35 -0400

    initial commit

---
+++ lunch.sh
@@ -0,0 +1,29 @@
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 100 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 200 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 300 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 400 0.001 tmp.pkl RNN categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 6 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 6 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 6 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 6 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 9 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 9 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 9 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 9 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 12 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 12 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 12 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 12 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 15 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 15 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 15 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 15 400 0.001 tmp.pkl RNNrsc categorical True CE
---
+++ lunch3.sh
@@ -0,0 +1,29 @@
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 100 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 200 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 300 0.001 tmp.pkl RNN categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 3 400 0.001 tmp.pkl RNN categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 10 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 10 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 10 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 10 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 20 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 20 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 20 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 20 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 30 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 30 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 30 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 30 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 40 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 40 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 40 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 40 400 0.001 tmp.pkl RNNrsc categorical True CE
+
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 50 100 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 50 200 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 50 300 0.001 tmp.pkl RNNrsc categorical True CE
+THEANO_FLAGS=device=gpu,floatX=float32 python memory_problem.py 1000 20 512 50 400 0.001 tmp.pkl RNNrsc categorical True CE
